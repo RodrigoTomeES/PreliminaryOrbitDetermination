4f2242cdcc69f2c88a48f19d11892b341fa0a2ce
WaterKnight1998 <dvdlacallecastillo@gmail.com>
2019-05-09 18:45:57 +0200

    Doubler function finalized.

--- doubler.h
+++ doubler.h
@@ -1,3 +1,3 @@
 #include <math.h>
 #include "MatLabUtilites.h"
-void doubler(double cc1, double cc2, double magrsite1, double magrsite2, double magrlin, double magr2in, double los1[], double los2[], double los3[], double rsite1[],double rsite2[], double rsite3[], double t1, double t3, char direct);
+void doubler(double cc1, double cc2, double magrsite1, double magrsite2, double magrlin, double magr2in, double los1[], double los2[], double los3[], double rsite1[],double rsite2[], double rsite3[], double t1, double t3, char direct, double r2 [], double r3 [], double * f1,double *f2,double *q1,double * magr1, double * magr2,double *a , double * deltae32);
