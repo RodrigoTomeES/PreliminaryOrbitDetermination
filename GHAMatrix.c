2e4daaab9780af6bef95f6c019c11464c278a84a
Rodrigo Tomé Nieto <tomecafeconrodrigo@live.com>
2019-05-17 20:55:46 +0200

    Added test to gast and GHAMatrix, bug fixed in SAT_Const

--- GHAMatrix.c
+++ GHAMatrix.c
@@ -1,5 +1,7 @@
 #include "GHAMatrix.h"
 
 void GHAMatrix(double Mjd_UT1, double ** eop, double filas,double columnas,double GHAmat[ROWS][COLS]){
-    R_z(gast(Mjd_UT1,eop,filas,columnas),GHAmat);
+	double gast_result = gast(Mjd_UT1,eop,filas,columnas);
+	printf("%f\n", gast_result);
+    R_z(gast_result,GHAmat);
 }
